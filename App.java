f21fc2be901444b22563bcdcf604fd118fe57c3b
Sergiy Poddubka <poseal@gmail.com>
2024-02-27 14:15:24 +0200

    solved task

---
+++ App.java
@@ -0,0 +1,142 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        BigDecimal min = null;
+        BigDecimal max = null;
+        BigDecimal sum = BigDecimal.ZERO;
+        List<BigDecimal> numbers = new LinkedList<>();
+
+        // Incr
+        LinkedList<BigDecimal> longestIncrSeq = new LinkedList<>();
+        LinkedList<BigDecimal> currentIncrSeq = new LinkedList<>();
+
+        // Decr
+        LinkedList<BigDecimal> longestDecrSeq = new LinkedList<>();
+        LinkedList<BigDecimal> currentDecrSeq = new LinkedList<>();
+
+        if (args.length > 0) {
+            String fileName = args[0];
+            File file = new File(fileName);
+            if (file.exists() && file.isFile()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        try {
+                            BigDecimal number = BigDecimal.valueOf(Double.parseDouble(line.trim()));
+                            if (min == null) min = number;
+                            if (max == null) max = number;
+                            // MIN
+                            if (number.compareTo(min) <= 0) {
+                                min = number;
+                            }
+                            // MAX
+                            if (number.compareTo(max) > 0) {
+                                max = number;
+                            }
+                            // SUM
+                            sum = sum.add(number);
+
+                            numbers.add(number);
+
+                            // Incr
+                            if (currentIncrSeq.isEmpty() || number.compareTo(currentIncrSeq.getLast()) > 0) {
+                                currentIncrSeq.add(number);
+                            } else {
+                                if (currentIncrSeq.size() > longestIncrSeq.size()) {
+                                    longestIncrSeq = new LinkedList<>(currentIncrSeq);
+                                }
+                                currentIncrSeq.clear();
+                                currentIncrSeq.add(number);
+                            }
+
+                            if (currentIncrSeq.size() > longestIncrSeq.size()) {
+                                longestIncrSeq = new LinkedList<>(currentIncrSeq);
+                            }
+
+                            // Decr
+                            if (currentDecrSeq.isEmpty() || number.compareTo(currentDecrSeq.getLast()) < 0) {
+                                currentDecrSeq.add(number);
+                            } else {
+                                if (currentDecrSeq.size() > longestDecrSeq.size()) {
+                                    longestDecrSeq = new LinkedList<>(currentDecrSeq);
+                                }
+                                currentDecrSeq.clear();
+                                currentDecrSeq.add(number);
+                            }
+
+                            if (currentDecrSeq.size() > longestDecrSeq.size()) {
+                                longestDecrSeq = new LinkedList<>(currentDecrSeq);
+                            }
+
+                        } catch (NumberFormatException e) {
+                            System.err.println("NumberFormatException! :" + e.getMessage());
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Something went wrong!");
+                }
+                int size = numbers.size();
+                // MAX
+                System.out.println("max: " + max);
+                // MIN
+                System.out.println("min: " + min);
+                // MEDIAN
+                printMedian(numbers);
+                // AVERAGE
+                System.out.println("average: " + sum.divide(BigDecimal.valueOf(size), 2, RoundingMode.UP));
+                // the largest increasing sequence
+                printList("the largest increasing sequence", longestIncrSeq);
+                // the largest decreasing sequence
+                printList("the largest decreasing sequence", longestDecrSeq);
+
+                long end = System.currentTimeMillis();
+                System.out.println();
+                System.out.println("Program running time: " + (end - start) / 1000 + " sec");
+            } else {
+                System.err.println("Wrong filename");
+            }
+        } else {
+            System.err.println("File not found");
+        }
+    }
+
+    private static void printMedian(List<BigDecimal> numbers) {
+        List<BigDecimal> sortedList = new ArrayList<>(numbers);
+        Comparator<BigDecimal> comparator = Comparator.naturalOrder();
+        sortedList.sort(comparator);
+        int sortedSize = sortedList.size();
+        int half = sortedSize / 2;
+        if (sortedSize % 2 == 0) {
+            BigDecimal result = (sortedList.get(half).add(sortedList.get(half - 1)))
+                    .divide(BigDecimal.valueOf(2), 2, RoundingMode.UP);
+            System.out.println("median: " + result);
+
+        } else {
+            System.out.println("median: " + sortedList.get(half));
+        }
+    }
+
+    private static void printList(String name, List<BigDecimal> list) {
+        System.out.println(name + "(" + list.size() + "):");
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                System.out.println(list.get(i));
+            } else {
+                System.out.print(list.get(i) + ", ");
+            }
+        }
+    }
+}
+
